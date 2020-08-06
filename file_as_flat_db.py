fda9e887ffe8ff4c8a492c83828c9e4933646ac0
Ed Mamerto <edmamerto@gmail.com>
2020-08-05 20:49:07 -0400

    add find_files.py

--- file_as_flat_db.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-	def __init__(slef, name, email_address, title, phone_number=None, identifier=None):
-		self.name = name
-		self.email_address = email_address
-		self.title = title
-		self.phone_number = phone_number
-
-	def email_signature(self, include_phone=False):
-		email_signature = f"{self.name} - {self.title}\n{self.email_address}"
